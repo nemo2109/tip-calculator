b92edde6e116e38e9f622854ccb37162ce55ed48
nemo2109 <114691858+nemo2109@users.noreply.github.com>
2022-12-15 00:45:12 -0600

    Logica de la aplicacion finalizada

---
+++ Js/main.js
@@ -0,0 +1,15 @@
+import tipCalculator from './modules/tipCalculator.js';
+import totalFinal from './modules/totalFinal.js';
+
+//Guardar valores en variables
+let subtotal = 142.55;
+let porcenTip = 0.15;
+let personas = 5;
+
+const resultadoTip = tipCalculator(subtotal, porcenTip, personas);
+
+console.dir(resultadoTip.toFixed(2));
+
+const resultadoFinal = totalFinal(subtotal, personas, resultadoTip);
+
+console.log(resultadoFinal.toFixed(2));
