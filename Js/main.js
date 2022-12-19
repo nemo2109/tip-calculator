f674b1763a45c07a23e9afa6593ebe570ca4b0a1
nemo2109 <114691858+nemo2109@users.noreply.github.com>
2022-12-18 18:31:23 -0600

    Finalizar actualizacion de los elementos

--- Js/main.js
+++ Js/main.js
@@ -1,34 +1,46 @@
 import tipCalculator from './modules/tipCalculator.js';
 import totalFinal from './modules/totalFinal.js';
 import updateTip from './modules/updateTip.js';
-import updateTotal from './modules/totalFinal.js';
+import updateTotal from './modules/updateTotal';
 
 //Constantes de los elemntos de HTML
 const main = document.getElementById('main');
 const form = document.getElementById('main-form');
 
-
 const tipResult = document.getElementById('result-tip');
 const totalResult = document.getElementById('result-total');
+
+//Custom box
+const customTip = document.getElementById('custom-tip');
 
 //Guardar valores en variables
 const subtotal = document.getElementById('total-bill');
 const porcenDiv = document.getElementById('form-btns');
 const personas = document.getElementById('total-people');
 
+//Metodo para seleccionar todos los botones
+const btns = document.querySelectorAll('.form__btns__button');
+const resetBtn = document.getElementById('reset-btn')
+
 //Variable para almacenar el valor porcentual seleccionado
 let porcenTip;
+let custom;
 
 // Evento para escuchar a que boton le estamos dando click
 porcenDiv.addEventListener('click', (e) => {
-  porcenTip = e.target;
-  console.log(porcenTip);
-
-  // Condicion para escuchar al custom
-  if (e.target.id === 'custom-tip') {
-    console.log('Si soy custom');
+  for(let i = 0; i < btns.length; i++){
+    btns[i].classList.remove('active');
   }
 
+  porcenTip = e.target;
+  porcenTip.classList.add('active');
+
+  // Condicion para escuchar al custom
+  if (e.target === custom-tip) {
+    custom = e.target;
+
+    custom.classList.remove('active')
+  }
 });
 
 // Crear un evento de submit para la forma
@@ -56,3 +68,18 @@ form.addEventListener('submit', (e) => {
   updateTip(tipFinal, tipResult);
   updateTotal(totalF, totalResult);
 });
+
+//Boton para reiniciar la tip calculator
+resetBtn.addEventListener('click', (e) => {
+  subtotal.value = '';
+  customTip.value = '';
+  personas.value = '';
+
+  for(let i = 0; i < btns.length; i++){
+    btns[i].classList.remove('active');
+  }
+
+  tipResult.innerText = '$0.00';
+  totalResult.innerText = '$0.00';
+});
+
